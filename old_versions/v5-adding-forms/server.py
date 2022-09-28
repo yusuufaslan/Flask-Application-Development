71ff892471e0135887348107d5bf9431f2a64646
Yusuf Aslan <yusuufaslan19@gmail.com>
2022-09-29 00:19:12 +0300

    old-versions-folder was updated.

---
+++ old_versions/v5-adding-forms/server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import views
+from database import Database
+from movie import Movie
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings")
+
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/movies", view_func=views.movies_page, methods=["GET", "POST"])
+    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+    app.add_url_rule("/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"])
+
+    db = Database()
+    # db.add_movie(Movie("Slaughterhouse-Five", year=1972))
+    # db.add_movie(Movie("The Shining"))
+    app.config["db"] = db
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    port = app.config.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
